2b2b5d7aca9a25f1d98e8b06305f3953c872261a
andrewhahm914 <andrewhahm14@gmail.com>
2025-03-11 22:31:02 +0000

    sdflgf

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -40,7 +40,8 @@ bool is_adjacent(const string& word1, const string& word2) {
 
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
     if (begin_word == end_word) {
-        return {};
+        return vector<string> empty;
+        return empty;
     }
 
     queue<vector<string>> ladder_q;
